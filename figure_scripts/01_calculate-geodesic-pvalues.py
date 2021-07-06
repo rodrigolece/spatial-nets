Preprint: fixed pvals calculation and finished scatter plots

figure_scripts/01_calculate-geodesic-pvalues.py
@@ -9,7 +9,8 @@ from spatial_nets import utils
 data_dir = Path(os.environ["workdirc"]) / "data"
 output_dir = Path("output_data")
 
-approx = True
+approx = False
+verbose = True
 
 dmat = utils.load_dmat(data_dir / "UK_geodesic_dmat.npz")
 data = utils.load_flows(data_dir / "UK_commute2011.npz")
@@ -17,17 +18,21 @@ locs = LocationsDataClass(data, coords=dmat)
 N = len(locs)
 
 print("\nComputing DC gravity model\n")
-grav = GravityModel(constraint="doubly", verbose=False)
+grav = GravityModel(constraint="doubly", verbose=verbose)
 prediction = grav.fit_transform(locs)
-doubly = DoublyConstrained(approx_pvalues=approx, verbose=True)
+doubly = DoublyConstrained(approx_pvalues=approx, verbose=verbose)
 norm_prediction = doubly.fit_transform(locs, prediction)
 grav_dc_pvals = doubly.pvalues()
 if grav_dc_pvals.N == N:
     grav_dc_pvals.model = "gravity"
     grav_dc_pvals.constraint = "doubly"
     grav_dc_pvals.approx_pvalues = approx
-    grav_dc_pvals.save(output_dir / "pvalues_geo_grav_doubly.pkl")
-    np.save(output_dir / "model_geo_grav_doubly.npy", norm_prediction)
+    grav_dc_pvals.save(
+        output_dir / f"pvalues{'-a' if approx else ''}_geo_grav_doubly.pkl"
+    )
+    model_filename = output_dir / "model_geo_grav_doubly.npy"
+    if not os.path.exists(model_filename):
+        np.save(model_filename, norm_prediction)
 else:
     print("something went wrong")
 
@@ -35,15 +40,17 @@ else:
 print("\nComputing PC radiation model\n")
 rad = RadiationModel(finite_correction=False)
 prediction = rad.fit_transform(locs)
-prod = ProductionConstrained(approx_pvalues=approx, verbose=True)
+prod = ProductionConstrained(approx_pvalues=approx, verbose=verbose)
 norm_prediction = prod.fit_transform(locs, prediction)
 rad_pc_pvals = prod.pvalues()
 if rad_pc_pvals.N == N:
     rad_pc_pvals.model = "gravity"
     rad_pc_pvals.constraint = "doubly"
     rad_pc_pvals.approx_pvalues = approx
-    rad_pc_pvals.save(output_dir / "pvalues_geo_rad_prod.pkl")
-    np.save(output_dir / "model_geo_rad_prod.npy", norm_prediction)
+    rad_pc_pvals.save(output_dir / f"pvalues{'-a' if approx else ''}_geo_rad_prod.pkl")
+    model_filename = output_dir / "model_geo_rad_prod.npy"
+    if not os.path.exists(model_filename):
+        np.save(model_filename, norm_prediction)
 else:
     print("something went wrong")
 

figure_scripts/02_plot-geodesic-scatterplots.py
@@ -10,7 +10,7 @@ from spatial_nets import LocationsDataClass
 from spatial_nets import utils
 from spatial_nets import draw
 
-from config import SAVEFIG, FORMAT
+from config import SAVEFIG, LEGEND, FORMAT
 
 
 # Change the default colors
@@ -19,23 +19,37 @@ custom_cycler = cycler(color=(draw.named_colors[c] for c in colors))
 plt.rcParams.update({"axes.prop_cycle": custom_cycler})
 
 
-data_dir = Path(os.environ["workdirc"]) / "data"
+raw_data_dir = Path("raw")
+data_dir = Path("output_data")
 output_dir = Path("output_figures")
 
-dmat = utils.load_dmat(data_dir / "UK_geodesic_dmat.npz")
-data = utils.load_flows(data_dir / "UK_commute2011.npz")
+dmat = utils.load_dmat(raw_data_dir / "UK_geodesic_dmat.npz")
+data = utils.load_flows(raw_data_dir / "UK_commute2011.npz")
 locs = LocationsDataClass(data, coords=dmat)
+
 
 print("\nLoading DC gravity data\n")
 descriptor = "geo_grav_doubly"
-prediction = np.load(output_dir / f"model_{descriptor}.npy")
-with open(output_dir / f"pvalues_{descriptor}.pkl", "rb") as f:
+
+prediction = np.load(data_dir / f"model_{descriptor}.npy")
+
+with open(data_dir / f"pvalues_{descriptor}.pkl", "rb") as f:
     pvals = pickle.load(f)
     pvals.set_significance(0.01)
+
+with open(data_dir / f"pvalues-a_{descriptor}.pkl", "rb") as f:
+    pvals_a = pickle.load(f)
+    pvals_a.set_significance(0.01)
 
 # First plot
 fig, ax = plt.subplots(figsize=(5, 5))
 draw.signed_scatterplot(locs, prediction, pvals, ax, verbose=True)
+draw.critical_enveloppes(locs, prediction, pvals_a, ax, verbose=True)
+if LEGEND:
+    leg = ax.legend()
+    for lh in leg.legendHandles:
+        lh._legmarker.set_alpha(1)
+
 if SAVEFIG:
     filename = f"scatter_{descriptor}.{FORMAT}"
     print("Writing: ", filename)
@@ -44,15 +58,26 @@ if SAVEFIG:
 
 print("\nLoading PC Radiation data\n")
 descriptor = "geo_rad_prod"
-prediction = np.load(output_dir / f"model_{descriptor}.npy")
-with open(output_dir / f"pvalues_{descriptor}.pkl", "rb") as f:
+
+prediction = np.load(data_dir / f"model_{descriptor}.npy")
+
+with open(data_dir / f"pvalues_{descriptor}.pkl", "rb") as f:
     pvals = pickle.load(f)
     pvals.set_significance(0.01)
 
+with open(data_dir / f"pvalues-a_{descriptor}.pkl", "rb") as f:
+    pvals_a = pickle.load(f)
+    pvals_a.set_significance(0.01)
 
 # Second plot
 fig, ax = plt.subplots(figsize=(5, 5))
 draw.signed_scatterplot(locs, prediction, pvals, ax, verbose=True)
+draw.critical_enveloppes(locs, prediction, pvals_a, ax, verbose=True)
+if LEGEND:
+    leg = ax.legend()
+    for lh in leg.legendHandles:
+        lh._legmarker.set_alpha(1)
+
 if SAVEFIG:
     filename = f"scatter_{descriptor}.{FORMAT}"
     print("Writing: ", filename)

figure_scripts/07_plot-uk-commuting.py
@@ -17,10 +17,12 @@ set3.pop(1)  # the same Peixoto does in graph-tool
 cm = colors.LinearSegmentedColormap.from_list("gt-Set3", set3)
 
 
+raw_data_dir = Path("raw")
 data_dir = Path("output_data")
 output_dir = Path("output_figures")
 
-coords = np.load(data_dir / "UK_centroids.npz")["longlat"]
+coords = np.load(raw_data_dir / "UK_centroids.npz")["longlat"]
+
 #  b = np.load(data_dir / "b_geo_grav-doubly_li_10.npy")
 #  b = np.load(data_dir / "b_geo_grav-doubly_lc_10.npy")
 b = np.load(data_dir / "b_geo_grav-doubly_wc_10.npy")

spatial_nets/draw.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib import cm
 from matplotlib import colors
@@ -121,13 +122,12 @@ def signed_scatterplot(
     T_model: np.ndarray,
     pvals: PValues,
     ax,
-    alpha=0.2,
-    fs=18,
-    verbose=False,
+    alpha: float = 0.2,
+    fs: float = 18,
+    verbose: bool = False,
     #  rounded=False,
     #  threshold=None,
 ):
-
     data = locs.flow_data
     pvals.verbose = verbose  # useful for debugging
     sig_plus, sig_minus = pvals.compute_backbone()
@@ -166,6 +166,38 @@ def signed_scatterplot(
     ax.set_ylabel("Predicted flow", fontsize=fs)
 
     return None
+
+
+def critical_enveloppes(
+    locs: LocationsDataClass,
+    T_model: np.ndarray,
+    pvals: PValues,
+    ax,
+    alpha: float = 0.5,
+    verbose: bool = False,
+):
+    data = locs.flow_data
+    pvals.verbose = verbose  # useful for debugging
+    sig_plus, sig_minus = pvals.compute_backbone()
+
+    p_obs = np.asarray(data[sig_plus.nonzero()]).flatten()
+    p_pred = T_model[sig_plus.nonzero()]
+    p_df = pd.DataFrame({"x": p_obs, "y": p_pred})
+    p_df = p_df.groupby("x")["y"].max().sort_index()
+    idx = ~(p_df < p_df.cummax())
+    top = p_df.loc[idx]
+
+    m_obs = np.asarray(data[sig_minus.nonzero()]).flatten()
+    m_pred = T_model[sig_minus.nonzero()]
+    m_df = pd.DataFrame({"x": m_obs, "y": m_pred})
+    m_df = m_df.groupby("x")["y"].min().sort_index()[::-1]
+    idx = ~(m_df > m_df.cummin())
+    bottom = m_df.loc[idx]
+
+    ax.plot(top, ls="-", color="0.3", alpha=alpha, label=r"$H_0^*$ boundary")
+    ax.plot(bottom, ls="-", color="0.3", alpha=alpha)
+
+    return top, bottom
 
 
 def signed_distance_histogram(