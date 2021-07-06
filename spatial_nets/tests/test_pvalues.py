bde259f0ca46f99758e8767da5067381db47a228
Rodrigo L.C <rodrigolealc@ciencias.unam.mx>
2021-07-06 12:47:48 +0100

    Added tests for AttractionConstrained pvalues

--- spatial_nets/tests/test_pvalues.py
+++ spatial_nets/tests/test_pvalues.py
@@ -8,7 +8,13 @@ from scipy.stats import norm, binom
 
 from spatial_nets.base import DataNotSet
 from spatial_nets.locations import LocationsDataClass
-from spatial_nets.models.constraints import ProductionConstrained
+from spatial_nets.models.constraints import (
+    ProductionConstrained,
+    AttractionConstrained,
+    #  DoublyConstrained,
+)
+
+# TODO: still need to test doubly constrained
 
 
 class TestPvalues(unittest.TestCase):
@@ -30,6 +36,15 @@ class TestPvalues(unittest.TestCase):
                 [1, 1, 0],
             ]
         )
+        model = ProductionConstrained()  # default: approx_pvalues=False
+        model.fit(self.locs).transform(self.prediction)
+        self.pvals = model.pvalues()
+
+        # Attraction: tested with transposed data
+        locs_a = LocationsDataClass(flow_data.T, coords=coords)
+        model = AttractionConstrained()
+        model.fit(locs_a).transform(self.prediction.T)
+        self.pvals_a = model.pvalues()
 
     def test_approx(self):
         model = ProductionConstrained(approx_pvalues=True)
@@ -46,50 +61,53 @@ class TestPvalues(unittest.TestCase):
         self.assertEqual(m[1, 0], 0.5)
 
     def test_exact(self):
-        model = ProductionConstrained()  # default: approx_pvalues=False
-        model.fit(self.locs).transform(self.prediction)
-        pvals = model.pvalues()
-        p, m = pvals.right, pvals.left
         one = binom(12, 3 / 4)
+        one_pval_r = one.pmf(range(4, 13)).sum()
+        one_pval_l = one.pmf(range(5)).sum()
         two = binom(12, 1 / 4)
-        self.assertTrue(
-            np.allclose(
-                p[0].toarray(),
-                [0, one.pmf(range(4, 13)).sum(), two.pmf(range(8, 13)).sum()],
-            )
-        )
-        self.assertTrue(
-            np.allclose(
-                m[0].toarray(),
-                [0, one.pmf(range(5)).sum(), two.pmf(range(9)).sum()],
-            )
-        )
+        two_pval_r = two.pmf(range(8, 13)).sum()
+        two_pval_l = two.pmf(range(9)).sum()
+
+        # Production
+        p, m = self.pvals.right, self.pvals.left
+        self.assertTrue(np.allclose(p[0].toarray(), [0, one_pval_r, two_pval_r]))
+        self.assertTrue(np.allclose(m[0].toarray(), [0, one_pval_l, two_pval_l]))
         self.assertEqual(p[1, 0], 3 / 4)
         self.assertEqual(m[1, 0], 3 / 4)
 
-    def test_compute_not_significant(self):
-        model = ProductionConstrained()  # default: approx_pvalues=False
-        model.fit(self.locs).transform(self.prediction)
-        pvals = model.pvalues()
-        with self.assertRaises(DataNotSet):
-            pvals.compute_graph()
+        # Attraction, tested with transposed data
+        p, m = self.pvals_a.right, self.pvals_a.left
+        self.assertTrue(np.allclose(p.toarray()[:, 0], [0, one_pval_r, two_pval_r]))
+        self.assertTrue(np.allclose(m.toarray()[:, 0], [0, one_pval_l, two_pval_l]))
+        self.assertEqual(p[0, 1], 3 / 4)
+        self.assertEqual(m[0, 1], 3 / 4)
 
-        pvals.set_significance()  # defauls: 0.01
-        zero = pvals.compute_not_significant()
+    def test_compute_not_significant(self):
+        expected = np.array([[0, 0, 0], [1, 0, 1], [1, 1, 0]])
+
+        # Production
+        with self.assertRaises(DataNotSet):
+            self.pvals.compute_graph()
+        self.pvals.set_significance()  # defauls: 0.01
+        zero = self.pvals.compute_not_significant()
         self.assertTrue(sp.issparse(zero))
-        self.assertTrue(
-            np.allclose(zero.toarray().astype(int), [[0, 0, 0], [1, 0, 1], [1, 1, 0]])
-        )
+        self.assertTrue(np.allclose(zero.toarray().astype(int), expected))
+
+        # Attraction, tested with transposed data
+        with self.assertRaises(DataNotSet):
+            self.pvals_a.compute_graph()
+        self.pvals_a.set_significance()
+        zero = self.pvals_a.compute_not_significant()
+        self.assertTrue(sp.issparse(zero))
+        self.assertTrue(np.allclose(zero.toarray().astype(int), expected.T))
 
     def test_compute_graph(self):
-        model = ProductionConstrained()  # default: approx_pvalues=False
-        model.fit(self.locs).transform(self.prediction)
-        pvals = model.pvalues()
+        # Production
         with self.assertRaises(DataNotSet):
-            pvals.compute_graph()
+            self.pvals.compute_graph()
 
-        pvals.set_significance()  # defauls: 0.01
-        G = pvals.compute_graph()
+        self.pvals.set_significance()  # defauls: 0.01
+        G = self.pvals.compute_graph()
         self.assertEqual(G.num_vertices(), 3)
         self.assertEqual(G.num_edges(), 2)
 
@@ -101,3 +119,21 @@ class TestPvalues(unittest.TestCase):
         self.assertEqual(f.source(), G.vertex(0))
         self.assertEqual(f.target(), G.vertex(1))
         self.assertEqual(G.ep.weight[f], 0)  # negative or left edge
+
+        # Attraction, tested with transposed data
+        with self.assertRaises(DataNotSet):
+            self.pvals_a.compute_graph()
+
+        self.pvals_a.set_significance()  # defauls: 0.01
+        G = self.pvals_a.compute_graph()
+        self.assertEqual(G.num_vertices(), 3)
+        self.assertEqual(G.num_edges(), 2)
+
+        e, f = G.edges()  # for some reason this reverses the order compared to above
+        self.assertEqual(f.source(), G.vertex(2))
+        self.assertEqual(f.target(), G.vertex(0))
+        self.assertEqual(G.ep.weight[f], 1)  # positive or right edge
+
+        self.assertEqual(e.source(), G.vertex(1))
+        self.assertEqual(e.target(), G.vertex(0))
+        self.assertEqual(G.ep.weight[e], 0)  # negative or left edge
