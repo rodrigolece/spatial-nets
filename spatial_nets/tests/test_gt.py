2f8f0e2ae38aad21bcfa97493e277fea3e99fa03
Rodrigo L.C <rodrigolealc@ciencias.unam.mx>
2021-07-07 11:34:06 +0100

    test_gt.py: test for version 2.37

--- spatial_nets/tests/test_gt.py
+++ spatial_nets/tests/test_gt.py
@@ -3,8 +3,10 @@ import unittest
 import numpy as np
 import graph_tool.all as gt
 
+
 class TestGraphTool(unittest.TestCase):
     def test_nmi(self):
-        x = [0]*5 + [1]*5
-        self.assertTrue(np.isclose(gt.mutual_information(x, x, norm=True), 1.0))
-
+        x = [0] * 5 + [1] * 5
+        N = len(x)
+        nmi = gt.mutual_information(x, x, norm=True)
+        self.assertTrue(np.isclose(nmi * N, 1.0))
